b2d768cb378a7e6f2ac52b7cc9adb56e8b9c86cb
Justin <38072911+4wzy@users.noreply.github.com>
2023-04-02 22:12:52 +0100

    Halfway there with the functionality!

--- main.py
+++ main.py
@@ -3,26 +3,132 @@ import gui
 import sys
 
 
-def poker_round(game):
-    # Create the table's deck
-    game.board = []
-
-    # Handle the player indexes for the small blind and big blind
+def start_betting_round(game, first_player, last_player):
+    # Handle the blinds
     game.handle_blinds()
+
+    ui.displayBlinds(game.big_blind_player_index,
+                     game.small_blind_player_index, game.dealer_button_player_index)
+    ui.displayChips([player.chips for player in game.players])
+
+    current_player = first_player
+
+    current_highest_bet = game.big_blind_value
+
+    while not game.are_bets_equal():
+        while current_player != last_player:
+            player = game.players_in_round[current_player]
+
+            # Get input from the user TEMPORARY
+            print(f"Cards: {player.hand.cards}")
+            print(f"{player.name}: {player.chips} chips")
+            while True:
+                choice = input("Choose: ")
+
+                if choice == "fold":
+                    game.fold(current_player)
+                    break
+
+                # IMPORTANT NOTE: The check button will be replaced will the call button if the user can not check.
+                elif choice == "check":
+
+                    # Check if the current player's contribution to the pot is equal to the highest bet (check criteria)
+                    if game.player_bets[player.name] == current_highest_bet:
+                        # Check by moving to the next player (do nothing as the current_player is incremented at the end)
+                        break
+                    else:
+                        continue
+
+                elif choice == "call":
+                    call_amount = current_highest_bet - \
+                        game.player_bets[player.name]
+                    # If the player has enough chips to call, put the appropriate amount of chips in the pot
+                    if player.chips >= call_amount:
+                        # Call
+                        player.chips -= call_amount
+                        game.pot.add_chips(call_amount)
+                        game.player_bets[player.name] += call_amount
+                    else:
+                        # If the player DOES NOT have enough chips to call, go all in
+                        remaining_chips = player.chips
+                        game.pot.add_chips(
+                            remaining_chips)
+                        player.chips = 0
+                        game.player_bets[player.name] += remaining_chips
+                    break
+
+                elif choice == "raise":
+                    # If the player has enough money to raise, raise
+                    if player.chips > current_highest_bet - game.player_bets[player.name]:
+                        # The player should only be able to raise up to the total amount of chips that they have.
+                        # This could be implemented in the GUI as a slider, but for now
+                        # I will implement a while loop that just asks the player for a raise_amount until it is valid.
+                        raise_amount = player.chips + 1
+                        while raise_amount > player.chips or raise_amount < game.minimum_raise_amount:
+                            if raise_amount < game.minimum_raise_amount:
+                                print(
+                                    f"Raise amount should be at least {game.minimum_raise_amount}")
+                            raise_amount = int(input("Raise amount: "))
+
+                        current_highest_bet += raise_amount
+                        player.chips -= raise_amount
+                        game.pot.add_chips(raise_amount)
+                        game.player_bets[player.name] = current_highest_bet
+
+                    # Else if they don't have enough money to raise, tell them that they don't (this will cause the player's turn to be skipped,
+                    # so the option to raise must not be on the screen if the player does not have enough money to raise)
+                    else:
+                        print("You do not have enough money to raise.")
+                        continue
+                    break
+
+            # After they have made their choice, move to the next player to the left.
+            ui.displayChips([player.chips for player in game.players])
+            current_player = (
+                current_player + 1) % len(game.players_in_round)
+
+
+def start_poker_round(game):
+    game.reset_bets()
+
+    # The first player to act is the player to the left of the big blind, and the play is clockwise
+    first_player = game.big_blind_player_index
+    current_player = first_player
+
+    # Add the small blinds and big blinds contributions to the player pot contributions
+    game.player_bets[game.players[game.small_blind_player_index].name
+                     ] = game.small_blind_value
+    game.player_bets[game.players[game.big_blind_player_index].name
+                     ] = game.big_blind_value
+
+    start_betting_round(game, current_player,
+                        game.dealer_button_player_index + 1)
+    game.flop()
+    ui.displayBoard(game.get_board_cards())
+    # The last player is set to game.dealer_button_player_index + 1, as the value of the dealer button index is incremented when handle_blinds() is called at the start.
+    # And the last player should be the big blind player.
+    start_betting_round(game, current_player,
+                        game.dealer_button_player_index + 1)
+    game.turn_river()
+    ui.displayBoard(game.get_board_cards())
+    start_betting_round(game, current_player, game.dealer_button_player_index)
+    game.turn_river()
+    ui.displayBoard(game.get_board_cards())
+
+    game.distribute_pot()
+
+
+def start_poker(game):
+    game.board = []
 
     # Deal cards to every player
     for player in game.players:
         game.deal_cards(2, player)
 
-    # Display the cards and blinds
     current_cards = game.get_player_cards()
     ui.displayCards(current_cards)
-    ui.displayBlinds(game.big_blind_player_index,
-                     game.small_blind_player_index)
 
-    game.flop()
-    game.betting_round()
-    ui.displayBoard(game.get_board_cards())
+    start_poker_round(game)
 
 
 # Set up the Poker Game
@@ -38,7 +144,10 @@ ui.setupUi(MainWindow)
 MainWindow.show()
 
 # Run the first poker round
-poker_round(game)
+# Create the table's deck (community cards)
+
+ui.displayNames([player.name for player in game.players])
+start_poker(game)
 
 # Handle exitting the GUI
 sys.exit(app.exec_())
--- poker_game.py
+++ poker_game.py
@@ -11,6 +11,9 @@ class Card:
     def __str__(self):
         return f"{self.rank} {self.suit}"
 
+    def __repr__(self) -> str:
+        return f"{self.rank} {self.suit}"
+
 
 class Deck:
     def __init__(self):
@@ -47,14 +50,17 @@ class Game:
         self.players = [Player(name, starting_chips) for name in player_names]
         self.big_blind_value = 4
         self.small_blind_value = 2
-        self.big_blind_player_index = 0
-        self.small_blind_player_index = randint(0, len(self.players) - 1)
         self.pot = Pot()
         self.board = []
         self.deck = Deck()
         self.deck.shuffle()
         self.minimum_raise_amount = self.big_blind_value * 2
         self.player_bets = {}
+        self.players_in_round = self.players
+        self.big_blind_player_index = 0
+        self.dealer_button_player_index = randint(
+            0, len(self.players_in_round) - 1)
+        self.small_blind_player_index = 0
 
     def get_player_cards(self):
         # Create poker card images
@@ -82,19 +88,21 @@ class Game:
             player.add_card(card)
 
     def handle_blinds(self):
-        amount_of_players = len(self.players)
+        amount_of_players = len(self.players_in_round)
 
         # Increment the small and big blind indexes by 1. Wraps around to the start if needed
+        self.dealer_button_player_index = (
+            self.dealer_button_player_index + 1) % amount_of_players
         self.small_blind_player_index = (
-            self.small_blind_player_index + 1) % amount_of_players
+            self.dealer_button_player_index + 1) % amount_of_players
         self.big_blind_player_index = (
             self.small_blind_player_index + 1) % amount_of_players
 
         # Deducts the blind values from the appropriate player's chips and adds it to the pot
-        self.players[self.big_blind_player_index].chips -= self.big_blind_value
+        self.players_in_round[self.big_blind_player_index].chips -= self.big_blind_value
         self.pot.add_chips(self.big_blind_value)
 
-        self.players[self.small_blind_player_index].chips -= self.small_blind_value
+        self.players_in_round[self.small_blind_player_index].chips -= self.small_blind_value
         self.pot.add_chips(self.small_blind_value)
 
     def fold(self, player_index):
@@ -116,86 +124,6 @@ class Game:
         # Create variables to track each player's contribution to the pot and the current highest bet
         for player in self.players_in_round:
             self.player_bets[player.name] = 0
-
-    def betting_round(self):
-        self.reset_bets()
-
-        # The first player to act is the player to the left of the big blind, and the play is clockwise
-        self.players_in_round = self.players
-        first_player = (self.small_blind_player_index +
-                        1) % len(self.players)
-        current_player = first_player
-
-        while not self.are_bets_equal():
-            player = self.players_in_round[current_player]
-
-            # Add the small blinds and big blinds contributions to the player pot contributions
-            self.player_bets[self.players[self.small_blind_player_index]
-                             ] += self.small_blind_value
-            self.player_bets[self.players[self.big_blind_player_index]
-                             ] += self.big_blind_value
-            current_highest_bet = self.big_blind_value
-
-            # Get input from the user TEMPORARY
-            choice = input("Choose: ")
-
-            # If the current player chooses fold, then fold
-            if choice == "fold":
-                self.fold(current_player)
-
-            # If the current player can check and they choose check, check
-            elif choice == "check":
-
-                # Check if the current player's contribution to the pot is equal to the previous player's contribution (check criteria)
-                if self.player_bets[current_player] == current_highest_bet:
-                    # Check by moving to the next player (do nothing as the current_player is incremented at the end)
-                    pass
-
-            # If the current player calls, call. The previous_bet is the amount to call.
-            elif choice == "call":
-                call_amount = current_highest_bet - \
-                    self.player_bets[current_player]
-                # If the player has enough chips to call, put the appropriate amount of chips in the pot
-                if player.chips >= call_amount:
-                    # Call
-                    player.chips -= call_amount
-                    self.pot.add_chips(call_amount)
-                    self.player_bets[current_player] += call_amount
-                else:
-                    # If the player DOES NOT have enough chips to call, go all in
-                    remaining_chips = player.chips
-                    self.pot.add_chips(
-                        remaining_chips)
-                    player.chips = 0
-                    self.player_bets[current_player] += remaining_chips
-
-            # If the current player raises, raise
-            elif choice == "raise":
-                # If the player has enough money to raise, raise
-                if player.chips > current_highest_bet - self.player_bets[current_player]:
-                    # The player should only be able to raise up to the total amount of chips that they have.
-                    # This could be implemented in the GUI as a slider, but for now
-                    # I will implement a while loop that just asks the player for a raise_amount until it is valid.
-                    raise_amount = player.chips + 1
-                    while raise_amount > player.chips or raise_amount < self.minimum_raise_amount:
-                        if raise_amount < self.minimum_raise_amount:
-                            print(
-                                f"Raise amount should be at least {self.minimum_raise_amount}")
-                        raise_amount = int(input("Raise amount: "))
-
-                    current_highest_bet += raise_amount
-                    player.chips -= raise_amount
-                    self.pot.add_chips(raise_amount)
-                    self.player_bets[current_player] = current_highest_bet
-
-                # Else if they don't have enough money to raise, tell them that they don't (this will cause the player's turn to be skipped,
-                # so the option to raise must not be on the screen if the player does not have enough money to raise)
-                print("You do not have enough money to raise.")
-
-                # After they have made their choice, move to the next player to the left.
-            current_player = (first_player + 1) % len(self.players)
-
-        self.distribute_pot()
 
     def flop(self):
         for i in range(3):
